523044b5f4eee7c572a46e89a1794d85152a53db
Sahil Rajurkar <sahilrajurkar88@gmail.com>
2025-02-09 09:24:07 +0530

    prepared for deployment

--- expense_tracker_frontend/src/services/ExpenseService.js
+++ expense_tracker_frontend/src/services/ExpenseService.js
@@ -1,16 +1,32 @@
+// import axios from "axios";
+
+
+
+// const REST_API_BASE_URL=`${process.env.REACT_APP_API_URL}/expense-tracker`;
+
+// export const listExpense = () => axios.get(REST_API_BASE_URL + "/all-expenses");
+
+// export const createExpense = (expense) => axios.post(REST_API_BASE_URL,expense);
+
+// export const getExpense=(expenseId)=> axios.get(REST_API_BASE_URL + "/" + expenseId);
+
+// export const updateExpense = (expenseId,expense) => axios.put(REST_API_BASE_URL + "/" + expenseId,expense)
+
+// export const deleteExpense= (expenseId)=> axios.delete(REST_API_BASE_URL+"/"+expenseId);
+
 import axios from "axios";
 
-const REST_API_BASE_URL="http://localhost:8081/expense-tracker";
+const REST_API_BASE_URL = `${import.meta.env.VITE_API_URL}/expense-tracker`;
+
+console.log("API Base URL:", REST_API_BASE_URL); // Debugging line
 
 export const listExpense = () => axios.get(REST_API_BASE_URL + "/all-expenses");
 
-export const createExpense = (expense) => axios.post(REST_API_BASE_URL,expense);
+export const createExpense = (expense) => axios.post(REST_API_BASE_URL, expense);
 
-export const getExpense=(expenseId)=> axios.get(REST_API_BASE_URL + "/" + expenseId);
+export const getExpense = (expenseId) => axios.get(REST_API_BASE_URL + "/" + expenseId);
 
-export const updateExpense = (expenseId,expense) => axios.put(REST_API_BASE_URL + "/" + expenseId,expense)
+export const updateExpense = (expenseId, expense) => axios.put(REST_API_BASE_URL + "/" + expenseId, expense);
 
-export const deleteExpense= (expenseId)=> axios.delete(REST_API_BASE_URL+"/"+expenseId);
-
-
+export const deleteExpense = (expenseId) => axios.delete(REST_API_BASE_URL + "/" + expenseId);
 
